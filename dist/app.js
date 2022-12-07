f81f484ab25ca44c0f773125660355cd88e36ba0
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-12-07 13:00:48 +0600

    primitive and non primitive

--- dist/app.js
+++ dist/app.js
@@ -2,3 +2,11 @@
 var message = "helo typescript";
 var message2 = "javaScript biday typescript 2";
 console.log(message, message2, language);
+// primitive type(string, boolean,number)
+var myName = "Mahamodul Hasan Moon";
+myName = 'Mahamodul';
+// uniontype(how infired)
+var myUnionType;
+myUnionType = 20;
+myUnionType = 40;
+myUnionType = 50;
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
 const message = "helo typescript"
 const message2 = "javaScript biday typescript 2"
 console.log(message,message2,language)
+
+// primitive type(string, boolean,number)
+
+
+let myName:string = "Mahamodul Hasan Moon"
+
+myName = 'Mahamodul'
+// uniontype(how infired)
+let myUnionType:number ;
+myUnionType=20
+
+myUnionType = 40
+
+myUnionType = 50
+
